5039861d119e66e1bd91ce9071c05065676cd9ca
Leon Walpole <leonw1020@gmail.com>
2017-07-09 11:36:47 +0900

    added initial files

---
+++ myscript.js
@@ -0,0 +1,109 @@
+// $('#draggableFox').draggable({
+//   containment: '#content',
+//   stack: ".drag, .drop",
+//   cursor: 'move',
+//   revert: true
+// });
+//
+// $('#draggableCamel').draggable({
+//   containment: '#content',
+//   stack: ".drag, .drop",
+//   cursor: 'move',
+//   revert: true
+// });
+//
+// $('#draggableLizard').draggable({
+//   containment: '#content',
+//   stack: ".drag, .drop",
+//   cursor: 'move',
+//   revert: true
+// });
+//
+// $('#draggableBeetle').draggable({
+//   containment: '#content',
+//   stack: ".drag, .drop",
+//   cursor: 'move',
+//   revert: true
+// });
+//
+// $('#draggableMeerkat').draggable({
+//   containment: '#content',
+//   stack: ".drag, .drop",
+//   cursor: 'move',
+//   revert: true
+// });
+
+$("button").click(showMessage);
+
+$("#success").hide();
+$("#success").css({
+  width: 0,
+  height: 0,
+});
+
+$('.drag').draggable({
+  containment: '#content',
+  stack: ".drag, .drop",
+  cursor: 'move',
+  revert: true
+});
+
+
+$('#droppableFox').droppable({
+  drop: handleDropEvent,
+  accept: "#draggableFox",
+  hoverClass: "hovered"
+});
+
+$('#droppableCamel').droppable({
+  drop: handleDropEvent,
+  accept: "#draggableCamel",
+  hoverClass: "hovered"
+});
+
+$('#droppableLizard').droppable({
+  drop: handleDropEvent,
+  accept: "#draggableLizard",
+  hoverClass: "hovered"
+});
+
+$('#droppableBeetle').droppable({
+  drop: handleDropEvent,
+  accept: "#draggableBeetle",
+  hoverClass: "hovered"
+});
+
+$('#droppableMeerkat').droppable({
+  drop: handleDropEvent,
+  accept: "#draggableMeerkat",
+  hoverClass: "hovered"
+});
+
+var animals = 5;
+var droppedAnimals = 0;
+
+function handleDropEvent(event, ui){
+  ui.draggable.draggable('disable');
+  ui.draggable.position({
+    of: $(this),
+    my: "left top",
+    at: "left top"
+  });
+  ui.draggable.draggable('option', 'revert', false);
+  droppedAnimals += 1;
+  if(droppedAnimals === animals){
+    console.log("Game Over!");
+    showMessage();
+  }
+}
+
+function showMessage(){
+  $("#success").show();
+  $("#success").animate({
+    width: "600px",
+    height: "140px",
+    bottom: "300px",
+    left: "240px",
+    opacity: 1
+  });
+}
